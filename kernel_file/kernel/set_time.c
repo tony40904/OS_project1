61e201f1a2cc1a8f179418f0a4694b4134cbb915
tonyxiao <r08922119@g.ntu.edu.tw>
2020-04-30 05:15:26 +0800

    completed

---
+++ kernel_file/kernel/set_time.c
@@ -0,0 +1,22 @@
+#include <linux/syscalls.h>
+#include <linux/kernel.h>
+#include <linux/ktime.h>
+#include <linux/timekeeping.h>
+#include <linux/linkage.h>
+#include <uapi/linux/time.h>
+
+SYSCALL_DEFINE3(set_time, int, isStart, int, pid, struct timespec*, timestamp)
+{    		
+	struct timespec now;
+	getnstimeofday(&now);
+
+	if (isStart)
+		copy_to_user(timestamp, &now, sizeof(now));
+	else
+	{
+		struct timespec startTime;
+		copy_from_user(&startTime, timestamp, sizeof(now));
+		printk("[Project1] %d %lu.%09lu %lu.%09lu\n", pid, startTime.tv_sec, startTime.tv_nsec, now.tv_sec, now.tv_nsec);
+	}
+	return 0;
+}
